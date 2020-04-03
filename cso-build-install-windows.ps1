d481a0c3cd5b5042718f25f6b38fb72e59af34a8
Vlad Serhiienko <vlad.serhiienko@gmail.com>
2020-04-03 22:33:29 +0300

    Remove debug build, fix generated folder path.

--- cso-build-install-windows.ps1
+++ cso-build-install-windows.ps1
@@ -1,3 +1,2 @@
 cmake -G "Visual Studio 16 2019" -H"." -Bbuild_msbuild2019_win64
-cmake --build .\build_msbuild2019_win64\ --config Debug
 cmake --build .\build_msbuild2019_win64\ --config Release
--- cso-generate-scheme-windows.ps1
+++ cso-generate-scheme-windows.ps1
@@ -1,2 +1,2 @@
-.\bin\Windows\flatc.exe -o .include -c .\schemes\cso.fbs
-.\bin\Windows\flatc.exe -o .generated -c .\schemes\cso.fbs --gen-mutable
+.\bin\Windows\flatc.exe -o .\include -c .\schemes\cso.fbs
+.\bin\Windows\flatc.exe -o .\generated -c .\schemes\cso.fbs --gen-mutable
